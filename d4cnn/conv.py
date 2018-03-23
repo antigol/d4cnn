25ac029d2ee3c03723778d7afcd056a6826b76cc
Mario Geiger <geiger.mario@gmail.com>
2018-03-23 18:30:18 +0100

    add D4ConvIR

--- d4cnn/conv.py
+++ d4cnn/conv.py
@@ -44,3 +44,35 @@ def test_D4ConvRR(image, out_channels, kernel_size, padding):
         assert (x - y).std() / x.std() < 1e-6
 
     return xs, ys
+
+
+class D4ConvIR(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, **kwargs):
+        super().__init__()
+        self.kwargs = kwargs
+        self.weight = nn.Parameter(torch.randn(out_channels, in_channels, kernel_size, kernel_size))
+
+    def forward(self, x):  # pylint: disable=W0221
+        # x [batch, channel, y, x]
+        assert x.dim() == 4
+
+        ws = image_all_actions(self.weight, 2, 3)
+
+        weight = torch.cat(ws, dim=0)
+
+        y = F.conv2d(x, weight, **self.kwargs)
+        y = y.view(x.size(0), 8, -1, y.size(2), y.size(3))
+        return y
+
+
+def test_D4ConvIR(image, out_channels, kernel_size, padding):
+    # image [batch, channel, y, x]
+    c = D4ConvIR(image.size(1), out_channels, kernel_size, padding=padding)
+
+    xs = field_all_actions(c(Variable(image)).data, 1, 3, 4)
+    ys = [c(Variable(gx)).data for gx in image_all_actions(image, 2, 3)]
+
+    for x, y in zip(xs, ys):
+        assert (x - y).std() / x.std() < 1e-6
+
+    return xs, ys
