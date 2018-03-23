4b4c90c34c8f3e0134b4755efa5e12bdb5f12ae7
Mario Geiger <geiger.mario@gmail.com>
2018-03-23 20:41:59 +0100

    D4ConvRI

--- d4cnn/conv.py
+++ d4cnn/conv.py
@@ -47,7 +47,8 @@ def test_D4ConvRR(image, out_channels, kernel_size, **kwargs):
     ys = [c(Variable(gx)).data for gx in field_all_actions(image, 1, 3, 4)]
 
     for x, y in zip(xs, ys):
-        assert (x - y).std() / x.std() < 1e-6
+        r = (x - y).std() / x.std()
+        assert r < 1e-5, repr(r)
 
     return xs, ys
 
@@ -80,6 +81,42 @@ def test_D4ConvIR(image, out_channels, kernel_size, **kwargs):
     ys = [c(Variable(gx)).data for gx in image_all_actions(image, 2, 3)]
 
     for x, y in zip(xs, ys):
-        assert (x - y).std() / x.std() < 1e-6
+        r = (x - y).std() / x.std()
+        assert r < 1e-5, repr(r)
+
+    return xs, ys
+
+
+class D4ConvRI(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, **kwargs):
+        super().__init__()
+        self.kwargs = kwargs
+        self.weight = nn.Parameter(uniform(out_channels, in_channels, kernel_size, kernel_size))
+        self.scale = (8 * in_channels * kernel_size ** 2) ** -0.5
+
+    def forward(self, x):  # pylint: disable=W0221
+        # x [batch, repr, channel, y, x]
+        assert x.dim() == 5
+        assert x.size(1) == 8
+
+        ws = image_all_actions(self.weight, 2, 3)
+
+        weight = torch.cat(ws, dim=1)
+
+        y = x.view(x.size(0), 8 * x.size(2), x.size(3), x.size(4))
+        y = F.conv2d(y, self.scale * weight, **self.kwargs)
+        return y
+
+
+def test_D4ConvRI(image, out_channels, kernel_size, **kwargs):
+    # image [batch, repr, channel, y, x]
+    c = D4ConvRI(image.size(2), out_channels, kernel_size, **kwargs)
+
+    xs = image_all_actions(c(Variable(image)).data, 2, 3)
+    ys = [c(Variable(gx)).data for gx in field_all_actions(image, 1, 3, 4)]
+
+    for x, y in zip(xs, ys):
+        r = (x - y).std() / x.std()
+        assert r < 1e-5, repr(r)
 
     return xs, ys
